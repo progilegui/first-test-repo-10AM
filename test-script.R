6e69e226e5d39552dc1fa2f8d9b3729a2e235ae8
progilegui <JCardoso@protonmail.com>
2025-05-16 10:16:14 -0700

    changing color scale

--- test-script.R
+++ test-script.R
@@ -1,2 +1,11 @@
 library(tidyverse)
 library(lterdatasampler)
+
+
+ggplot(data = pie_crab,
+       aes(x = latitude,
+           y = size,
+           color = latitude)) +
+  geom_point() + 
+  scale_color_gradient(low = "#749238",
+                       high = "#275834")
